85c98d32a01f822bb84f5ae4c638f7b63e09f83f
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-21 22:20:43 +0200

    chore: init of prettier

---
+++ src/components/Solution.tsx
@@ -0,0 +1,9 @@
+
+
+export const Solution = () => {
+
+    return <div>
+        This is soluton
+    </div>
+
+}
---
+++ src/components/StartingReactProject.tsx
@@ -0,0 +1,26 @@
+import logo from "../logo.svg";
+import React from "react";
+
+
+type Props = {
+    onAnchorClick: () => void
+}
+
+export const StartingReactProject = ({onAnchorClick}: Props) => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    onClick={onAnchorClick}
+                    className="App-link"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
