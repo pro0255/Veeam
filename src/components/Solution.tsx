ef2d7873f66e784edbfb04f47c1e45d160168a0d
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-23 23:43:00 +0200

    feat: added link to veeam

--- src/components/Solution.tsx
+++ src/components/Solution.tsx
@@ -4,10 +4,16 @@ import { FormEditor } from './FormEditor/FormEditor';
 import { FormRenderer } from './FormRenderer/FormRenderer';
 import { FormStorageProvider } from './FormStorage';
 
+const VEEAM_LINK = 'https://www.veeam.com/cz';
+
 export const Solution = () => {
   return (
     <>
-      <h1>This is Veeam Solution</h1>
+      <h1>
+        <a target={'_blank'} rel={'noopener noreferrer'} href={VEEAM_LINK}>
+          Veeam Solution
+        </a>
+      </h1>
       <FormStorageProvider>
         <TwoColumnLayout left={<FormEditor />} right={<FormRenderer />} />
       </FormStorageProvider>
