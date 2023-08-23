9344fc047f9d61f40530a564562c47ba9cb87454
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-23 20:43:10 +0200

    feat: FormEditor with apply button reacting on validation

--- src/components/FormEditor/FormEditor.tsx
+++ src/components/FormEditor/FormEditor.tsx
@@ -4,13 +4,29 @@ import { useFormEditor } from './hooks/useFormEditor';
 import { DEFAULT_EDITOR_VALUE } from './constants/default';
 
 export const FormEditor = () => {
-  const { validate } = useFormEditor();
+  const { validate, renderForm, saveEditorReference, isValid } = useFormEditor();
   return (
-    <Editor
-      height="50vh"
-      defaultLanguage="json"
-      defaultValue={JSON.stringify(DEFAULT_EDITOR_VALUE)}
-      onValidate={validate}
-    />
+    <>
+      <main>
+        <Editor
+          height="50vh"
+          defaultLanguage="json"
+          defaultValue={JSON.stringify(DEFAULT_EDITOR_VALUE)}
+          onValidate={validate}
+          onMount={saveEditorReference}
+        />
+      </main>
+      <footer>
+        <button
+          disabled={!isValid}
+          onClick={renderForm}
+          style={{
+            width: '100%',
+            minHeight: '40px'
+          }}>
+          Apply
+        </button>
+      </footer>
+    </>
   );
 };
--- src/components/FormEditor/hooks/useFormEditor.ts
+++ src/components/FormEditor/hooks/useFormEditor.ts
@@ -1,13 +1,29 @@
 import { initializeMonaco } from '../utils/initializeMonaco';
+import { useRef, useState } from 'react';
+import { OnMount, OnValidate } from '@monaco-editor/react';
 
 initializeMonaco();
 
+type EditorType = Parameters<OnMount>[0];
+
 export const useFormEditor = () => {
-  const validate = (markers: any) => {
-    // model markers
-    console.log(markers);
-    markers.forEach((marker: any) => console.log('onValidate:', marker.message));
+  const editorRef = useRef<EditorType | null>(null);
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  const validate: OnValidate = (markers) => {
+    const areEmpty = markers.length === 0;
+    setIsValid(areEmpty);
   };
 
-  return { validate };
+  const saveEditorReference: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const renderForm = () => {
+    if (editorRef.current) {
+      alert(editorRef.current.getValue());
+    }
+  };
+
+  return { validate, renderForm, saveEditorReference, isValid };
 };
