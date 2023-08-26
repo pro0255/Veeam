73d6140e385c642f3c4e9bc6b76c28b60e0bc02f
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-26 11:49:27 +0200

    feat: added a style for empty result page

--- src/components/FormRenderer/components/EmptyForm.tsx
+++ src/components/FormRenderer/components/EmptyForm.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const EmptyForm = () => {
-  return <div>Form content is pending...</div>;
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center text-gray-500 text-lg">
+      The form has not been presented yet. Please navigate to the Configuration section and apply
+      the form to observe its content.
+    </div>
+  );
 };
