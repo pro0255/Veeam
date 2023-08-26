fed5ffb33f7c072e3abd03529bb46b9d205446a7
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-26 12:02:50 +0200

    refactor: updated a styled in footer "Config" tab

--- src/components/FormEditor/FormEditor.tsx
+++ src/components/FormEditor/FormEditor.tsx
@@ -10,23 +10,18 @@ export const FormEditor = () => {
   const { validate, renderForm, saveEditorReference, isValid } = useFormEditor();
   return (
     <div className="flex flex-col w-full h-[100vh]">
-      <main className="w-full h-full max-h-[80%] p-10">
+      <main className="w-full h-full max-h-[90%]">
         <Editor
+          height="85vh"
           defaultLanguage="json"
           defaultValue={JSON.stringify(form ?? MORE_RADIO_BUTTONS_FORM)}
           onValidate={validate}
           onMount={saveEditorReference}
         />
       </main>
-      <footer className="p-5 border-t-2 border-gray-200 fixed bottom-0 w-[100vw] flex flex-row items-center justify-center">
-        <span className="w-[50%]">
-          <Button
-            disabled={!isValid}
-            onClick={renderForm}
-            style={{
-              width: '100%',
-              minHeight: '40px'
-            }}>
+      <footer className="p-5 border-t-2 border-gray-200 h-full flex flex-col items-center justify-center">
+        <span className="w-[25%]">
+          <Button disabled={!isValid} onClick={renderForm} className="w-full">
             Apply
           </Button>
         </span>
