f57117445106b83cbe99d670fe17ad0ec94e7784
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-26 16:35:59 +0200

    docs: added a description of FormFields rendering logic

--- src/components/FormRenderer/components/FormFields/FormFields.tsx
+++ src/components/FormRenderer/components/FormFields/FormFields.tsx
@@ -1,59 +1,77 @@
-import React, { ReactElement } from 'react'
-import { FieldRenderer } from './FieldRenderer'
-import { FieldSet } from './FieldSet'
-import { isRadioButton } from './isRadioButton'
-import { LabelWrapper } from './LabelWrapper'
-import { FormFieldsProps } from './types'
+import React, { ReactElement } from 'react';
+import { FieldRenderer } from './FieldRenderer';
+import { FieldSet } from './FieldSet';
+import { isRadioButton } from './isRadioButton';
+import { LabelWrapper } from './LabelWrapper';
+import { FormFieldsProps } from './types';
+
+const isLastRadioElement = (
+  type: FormFieldsProps['items'][number]['type'],
+  currentLoopIndex: number,
+  items: FormFieldsProps['items']
+) => {
+  return isRadioButton(type) && currentLoopIndex === items.length - 1;
+};
+
+const pushFieldSet = (target: ReactElement[], fieldSet: ReactElement[], fieldSetLabel: string) => {
+  target.push(
+    <li key={fieldSetLabel} className="mt-5">
+      <FieldSet label={fieldSetLabel}>{fieldSet}</FieldSet>
+    </li>
+  );
+};
+
+const pushFormElement = (
+  target: ReactElement[],
+  { label, type }: FormFieldsProps['items'][number],
+  setId: number
+) => {
+  target.push(
+    <li key={label} className="mt-5">
+      <LabelWrapper label={label} type={type}>
+        <FieldRenderer type={type} label={label} setId={setId} />
+      </LabelWrapper>
+    </li>
+  );
+};
 
 const createFormElements = (items: FormFieldsProps['items']): ReactElement[] => {
   const elements: ReactElement[] = [];
 
-  let radioSet = 0;
-  let renderingRadio = false;
+  // Logic which covers the "enum" topic. I want to have these elements wrapped in fieldset and give it a particular label. The correct approach would be edited a JSON into better structure.
+  let radioSetId = 0;
+  let isRenderingRadio = false;
+  // For now, it's "radioElements". Can be edited into "fieldSetElements".
   let radioElements: ReactElement[] = [];
 
   items.forEach(({ label, type }, index) => {
-    const radioSetLabel = `Radio set ${radioSet}`;
+    const radioSetLabel = `Radio set ${radioSetId}`;
+
     // Logic which wraps buttons into "fieldSet"
     if (isRadioButton(type)) {
-      renderingRadio = true;
+      isRenderingRadio = true;
     }
 
-    if (isRadioButton(type) && index === items.length - 1) {
-      elements.push(
-        <li key={radioSetLabel} className="mt-5">
-          <FieldSet label={radioSetLabel}>{radioElements}</FieldSet>
-        </li>
-      );
+    // Because last element could be radio without that logic would not be last radioButton fieldSet renderer properly
+    if (isLastRadioElement(type, index, items)) {
+      pushFieldSet(elements, radioElements, radioSetLabel);
     }
 
-    if (renderingRadio && !isRadioButton(type)) {
-      renderingRadio = false;
-      elements.push(
-        <li key={radioSetLabel} className="mt-5">
-          <FieldSet label={radioSetLabel}>{radioElements}</FieldSet>
-        </li>
-      );
+    const differentThenRadioAfterRadio = isRenderingRadio && !isRadioButton(type);
+
+    if (differentThenRadioAfterRadio) {
+      // Pushes "radioElements" as fieldSet into form elements
+      isRenderingRadio = false;
+      pushFieldSet(elements, radioElements, radioSetLabel);
       radioElements = [];
-      radioSet++;
+      radioSetId++;
     }
 
-    if (renderingRadio) {
-      radioElements.push(
-        <li key={label} className="mt-5">
-          <LabelWrapper label={label} type={type}>
-            <FieldRenderer type={type} label={label} setId={radioSet} />
-          </LabelWrapper>
-        </li>
-      );
+    if (isRenderingRadio) {
+      // The target (first argument) differs. The form element is pushed into "fieldSet array".
+      pushFormElement(radioElements, { label, type }, radioSetId);
     } else {
-      elements.push(
-        <li key={label} className="mt-5">
-          <LabelWrapper key={label} label={label} type={type}>
-            <FieldRenderer type={type} label={label} setId={radioSet} />
-          </LabelWrapper>
-        </li>
-      );
+      pushFormElement(elements, { label, type }, radioSetId);
     }
   });
 
